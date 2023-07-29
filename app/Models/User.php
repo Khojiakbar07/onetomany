7f258a808fd875d760effc5b93a0117b72a5ed66
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-07-29 23:18:50 +0500

    CODE RUB:One to Many CRUD

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Post;
 
 class User extends Authenticatable
 {
@@ -42,4 +43,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function post(){
+        return $this->hasMany(Post::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,43 @@
 <?php
 
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function () {
+    $user = User::findOrFail(1);
+
+    // $post = new Post(['title' => 'My first post with Edwin', 'body'=>'Laravel is Php framework']);
+
+    $user->posts()->save(new Post(['title' => 'My first post with Edwin', 'body'=>'Laravel is Php framework']));
+    
+});
+
+Route::get('/read', function() {
+    $user = User::findOrFail(1);
+
+    foreach($user->posts as $post){
+        echo $post->title . "<br>";
+    }
+});
+
+Route::get('/update', function() {
+    $user = User::findOrFail(1);
+
+    $user->posts()->where('id','=',2)->update(['title' => 'Portfoliom 2','body'=>'My name is Hojiakbar Zokirov and I`m Fullstack developer']);
+});
+
+
+
+Route::get('/delete', function() {
+    $user = User::findOrFail(1);
+
+    $user->posts()->whereId(1)->delete();
+
+    return "done";
 });
